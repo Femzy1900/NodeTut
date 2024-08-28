d08d45a251e50fc7a1cd21dc1bb3b5db9dad4c7a
Femzy1900 <adedokunfemi14@gmail.com>
2024-08-28 06:33:43 -0500

    Adding packages using npm

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,10 +2,7 @@ const http = require("http")
 const fs = require("fs");
 const url = require('url')
 const { dirname } = require("path");
-
-
-
-
+const replaceTemplate = require('./modules/replaceTemplate')
 
 
 /////////////////////////////////
@@ -36,19 +33,7 @@ const { dirname } = require("path");
 // console.log('Will read file!');
 
 // SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-    return output;
-}
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
@@ -72,8 +57,9 @@ const server = http.createServer((req, res) => {
 
     //Product
     } else if (pathname === '/product') {
-        console.log(query)
-        res.end(tempProduct )
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product)
+        res.end(output )
 
     //API
     } else if (pathname === '/api') {
