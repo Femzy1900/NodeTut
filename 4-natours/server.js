ed633db56a5de78a49ad1754b92703e6397f927c
Femzy1900 <adedokunfemi14@gmail.com>
2024-08-31 15:03:02 -0500

    prettier and eslint installation

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,12 +1,10 @@
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
-const app = require('./app')
-
-
+const app = require('./app');
 
 // SERVER PORT
 const port = process.env.PORT;
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
