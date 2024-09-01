68c9239f0f894f1c1f139d0181c5c4de41dfe2fc
Femzy1900 <adedokunfemi14@gmail.com>
2024-09-01 08:11:28 -0500

    connecting to mongodb

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,5 +1,34 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log('DB connection successful!!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+const Tour = mongoose.model('Tour', tourSchema)
 
 const app = require('./app');
 
