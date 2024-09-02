c42507ccd35dc93b79619efb92d515d02b7fb60b
Femzy1900 <adedokunfemi14@gmail.com>
2024-09-01 20:58:02 -0500

    creating the model folder

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -13,29 +13,6 @@ mongoose
     console.log('DB connection successful!!');
   });
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: "The Testing the hiker",
-  price: 500,
-  rating: 4.6
-})
-
 const app = require('./app');
 
 // SERVER PORT
